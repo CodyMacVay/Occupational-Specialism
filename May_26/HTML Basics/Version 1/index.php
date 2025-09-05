400f3485ea30564044fa14a740f3d3a6e7597f7f
Cody Mac Vay <cody.macvay1@gmail.com>
2025-09-05 13:10:57 +0100

    Started an html basics folder in main for exams may 26

--- May_26/HTML Basics/Version 1/index.php
+++ May_26/HTML Basics/Version 1/index.php
@@ -4,16 +4,28 @@ echo "<!DOCTYPE html>";  // Desired tag to declare the correct structure of an h
 
 echo "<html>";
 echo "<head>";  // Allows for a head to be created
-echo "<title>Love html</title>";  // Adds a title inside the head that was created in the line before
+    echo "<title>Love html</title>";  // Adds a title inside the head that was created in the line before
 
 echo "</head>";  // Closes the head on the website
 echo "<body>";  // Opens the body
 
-echo "<h1>Love CSS Aswell</h1>";  // Allows a header inside the body
+    echo "<h1>Love CSS Aswell</h1>";  // Allows a header inside the body
 
-echo "This is my page.";
+    echo "This is my page.";
 
-echo "<p><b><i> Nothing beats a jet2 holiday where right now you can save just 50 pound per person thats 200 pound for a family of 4 </p></b></i><hr>";
+    echo "<a href='page2.php'>Go to Page 2</a>";
 
+
+    echo "<table>";
+
+        echo "<tr>";
+            echo "<td> Hello1 </td>";
+            echo "<td> Hello2 </td>";
+            echo "<td> Hello3 </td>";
+        echo "</tr>";
+
+    echo "</table>";
+
+echo "</body>";
 
 ?>
---
+++ May_26/HTML Basics/Version 1/page2.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<html>";
+
+    echo "<head>";
+        echo "<h1><b></b> We love Homer Simpson </h1></b>";
+    echo "</head>";
+
+    echo "<body>";
+        echo "<a href='https://www.gettyimages.co.uk/photos/homer-simpson'> Click here for more </a>";
+        echo "<br>";
+        echo "<br>";
+        echo "<img src='homer.jfif'>";
+    echo "</body>";
+
+echo "</html>";
+
+?>
