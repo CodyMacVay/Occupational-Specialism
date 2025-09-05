bc4f1a62c67d60722be1c63585cca53e388546b5
Cody Mac Vay <cody.macvay1@gmail.com>
2025-09-05 10:38:42 +0100

    Started an html basics folder in main for exams may 26

---
+++ May_26/HTML Basics/Version 1/index.php
@@ -0,0 +1,14 @@
+<?php // Thos opens the php section
+
+echo "<html>";
+echo "<head>";  // Allows for a head to be created
+echo "<title>Love html</title>";  // Adds a title inside the head that was created in the line before
+
+echo "</head>";  // Closes the head on the website
+echo "<body>";  // Opens the body
+
+echo "<h1>Love CSS Aswell</h1>";  // Allows a header inside the body
+
+echo "This is my page.";
+
+?>
