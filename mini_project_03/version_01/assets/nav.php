02398048b453617bdcd95e8a7a6de44c12f045f9
Cody Mac Vay <cody.macvay1@gmail.com>
2025-09-18 13:51:08 +0100

    Mini project 03

---
+++ mini_project_03/version_01/assets/nav.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "<div class='nav'>";
+        echo "<nav>";
+            echo "<ul>";
+                echo "<li> <a href='index.php'>Home</a> </li>"; // Link to Home page.
+                echo "<li> <a href='passcheck.php'>Password Checker</a> </li>"; // Link to About page.
+            echo "</ul>";
+        echo "</nav>";
+    echo "</div>";
+?>
+
